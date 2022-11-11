7ed1f63d985f4dea806f3b69dc876ba63e769834
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-11-11 21:22:43 +0530

    created models and fetched data from backend

--- base/views.py
+++ base/views.py
@@ -1,27 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Lets learn python1'},
-    {'id': 3, 'name': 'Lets learn python2'},
-    {'id': 4, 'name': 'Lets learn python3'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Lets learn python1'},
+#     {'id': 3, 'name': 'Lets learn python2'},
+#     {'id': 4, 'name': 'Lets learn python3'},
+# ]
 
 
 def home(request):
-    return render(request, 'base/home.html', {'rooms': rooms})
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
+
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    """Updating the particular room form value"""
+
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
