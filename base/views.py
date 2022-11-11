7dcc5fdbf7064c6d8ee4f6954f062991aca2c0cc
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-11-11 20:03:47 +0530

    created templates

--- base/views.py
+++ base/views.py
@@ -4,9 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Lets learn python1'},
+    {'id': 3, 'name': 'Lets learn python2'},
+    {'id': 4, 'name': 'Lets learn python3'},
+]
+
+
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def room(request):
-    return HttpResponse("Room Page")
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
